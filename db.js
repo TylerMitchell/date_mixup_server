e324a0eb73f77edd269723f0b966168cad6263cf
Tyler Mitchell <tylermitchellisawesome@gmail.com>
2020-12-06 05:46:04 -0500

    changing database url for heroku

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DB_URL, {dialect: "postgres"});
+const db = new Sequelize(process.env.DATABASE_URL, {dialect: "postgres"});
 
 module.exports = db;
